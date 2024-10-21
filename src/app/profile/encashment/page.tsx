9aa89bde6493aeaa94e434a3e8d06c04ea2a1099
Kostya Naida <varkac69@gmail.com>
2024-10-21 15:21:49 +0300

    added encashment component

--- src/app/profile/encashment/page.tsx
+++ src/app/profile/encashment/page.tsx
@@ -1,5 +1,69 @@
-import React from 'react'
+import styles from './encashment.module.css'
 
 export default function EncashmentPage() {
-	return <div>Encash</div>
+	return (
+		<div className={styles.newEncashmentContainer}>
+			<h2 className={styles.newEncashmentTitle}>Нове винесення</h2>
+			<form action="" className={styles.newEncashmentForm}>
+				<div className={styles.newEncashmentInputsTitlesContainer}>
+					<h3>Номінал</h3>
+					<h3>Кількість купюр</h3>
+				</div>
+				<div className={styles.newEncashmentInputsContainer}>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							20грн
+						</label>
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							50грн
+						</label>
+
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							100грн
+						</label>
+
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							200грн
+						</label>
+
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							500грн
+						</label>
+
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+					<div className={styles.newEncashmentLabelContainer}>
+						<label htmlFor="" className={styles.newEncashmentLabel}>
+							1000грн
+						</label>
+
+						<input type="number" className={styles.newEncashmentInput} />
+					</div>
+				</div>
+				<div className={styles.newEncashmentFormBtns}>
+					<button
+						type="submit"
+						className={`${styles.btn} ${styles.btnSuccess}`}
+					>
+						Ок
+					</button>
+					<button type="reset" className={`${styles.btn} ${styles.btnCancel}`}>
+						Очистити
+					</button>
+				</div>
+			</form>
+		</div>
+	)
 }
