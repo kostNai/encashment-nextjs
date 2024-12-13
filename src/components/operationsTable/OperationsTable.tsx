f9b1d9f0dff75577a605bab59857532f72bf55d5
Kostya Naida <varkac69@gmail.com>
2024-12-13 23:49:24 +0200

    Added filter, sorting and search to operationsTable conponent

--- src/components/operationsTable/OperationsTable.tsx
+++ src/components/operationsTable/OperationsTable.tsx
@@ -1,18 +1,30 @@
 'use client'
+import { useEffect, useRef, useState } from 'react'
+import axios from 'axios'
+import { MdDeleteForever } from 'react-icons/md'
+import { IoCloseOutline } from 'react-icons/io5'
+import { CiSearch } from 'react-icons/ci'
+import { toast } from 'react-toastify'
+import 'react-toastify/ReactToastify.min.css'
+
 import { Operation } from '@/types'
 import styles from './OperatinosTable.module.css'
-import { MdDeleteForever } from 'react-icons/md'
-import axios from 'axios'
-import { useEffect, useRef, useState } from 'react'
 import ConfirmModal from '../UI/confirmModal/ConfirmModal'
-import { toast } from 'react-toastify'
 import { revalidateByPath } from '@/actions/actions'
-import 'react-toastify/ReactToastify.min.css'
+import { HiArrowsUpDown } from 'react-icons/hi2'
 
 type Props = {
     operations: Operation[]
     pharmacy_number?: number
 }
+const tableItems = [
+    { name: 'id', title: 'Id' },
+    { name: 'number', title: 'Номер винесення' },
+    { name: 'pharmacy_number', title: 'Номер аптеки' },
+    { name: 'total_sum', title: 'Сума' },
+    { name: 'created_at', title: 'Дата винесення' },
+    { name: 'action', title: 'Керування' },
+]
 
 export default function OperationsTable({
     operations,
@@ -20,6 +32,20 @@ export default function OperationsTable({
 }: Props) {
     const [confirm, setConfirm] = useState<boolean | undefined>(false)
     const [operationId, setOperationId] = useState<string | undefined>('')
+    const [isInputValueLength, setIsInputValueLength] = useState<
+        boolean | undefined
+    >(false)
+    const [sortingKey, setSortingKey] = useState<string | undefined>('')
+    const [sortedOperations, setSortedOperations] = useState<
+        Operation[] | undefined
+    >([])
+    const [filteredOperations, setFilteredOperations] = useState<
+        Operation[] | undefined
+    >([])
+    const [searchValue, setSearchValue] = useState<string | undefined>('')
+    const [isReverseSorting, setIsReverseSorting] = useState<
+        boolean | undefined
+    >(false)
     const dialogRef = useRef<HTMLDialogElement>(null)
 
     useEffect(() => {
@@ -29,6 +55,16 @@ export default function OperationsTable({
             dialogRef.current?.close()
         }
     }, [confirm])
+
+    useEffect(() => {
+        if (sortedOperations!.length < 1) setSortedOperations([...operations])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchValue, operations])
+
+    useEffect(() => {
+        setFilteredOperations([...operations!])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [operations])
 
     const onConfirmHandler = (id: string) => {
         setConfirm(true)
@@ -51,6 +87,35 @@ export default function OperationsTable({
     const onResetHandler = () => {
         setConfirm(false)
     }
+    const onSortingHandler = (key: string) => {
+        setSortingKey(key)
+        if (isReverseSorting) {
+            const sorted = sortedOperations?.sort((a: any, b: any) => {
+                return a[key] < b[key] ? 1 : a[key] > b[key] ? -1 : 0
+            })
+            setFilteredOperations([...sorted!])
+            setIsReverseSorting(!isReverseSorting)
+        } else {
+            const sorted = sortedOperations?.sort((a: any, b: any) => {
+                return a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0
+            })
+            setFilteredOperations([...sorted!])
+            setIsReverseSorting(!isReverseSorting)
+        }
+    }
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value)
+
+        if (e.target.value.length > 0) setIsInputValueLength(true)
+        else setIsInputValueLength(false)
+
+        const filtered = sortedOperations!.filter((operation: Operation) => {
+            return JSON.stringify(Object.values(operation)).includes(
+                e.target.value
+            )
+        })
+        setFilteredOperations([...filtered])
+    }
 
     return (
         <div>
@@ -67,32 +132,59 @@ export default function OperationsTable({
                     )}
                 </form>
             </dialog>
+            <div className={styles.operationsTableSearchContainer}>
+                <input
+                    type="text"
+                    className={styles.operationsTableSearchInput}
+                    placeholder="Пошук..."
+                    onChange={onChangeHandler}
+                    value={searchValue}
+                />
+                {isInputValueLength ? (
+                    <IoCloseOutline
+                        className={styles.operationsTableSearchIcon}
+                        size={24}
+                        // onClick={onResetSearchValueHandler}
+                    />
+                ) : (
+                    <CiSearch
+                        className={styles.operationsTableSearchIcon}
+                        size={24}
+                    />
+                )}
+            </div>
 
             <table className={styles.operationsTable}>
                 <thead className={styles.operationsTableHead}>
                     <tr>
-                        <th scope="col" className={styles.operationsTableTh}>
-                            Id
-                        </th>
-                        <th scope="col" className={styles.operationsTableTh}>
-                            Номер винесення
-                        </th>
-                        <th scope="col" className={styles.operationsTableTh}>
-                            Номер аптеки
-                        </th>
-                        <th scope="col" className={styles.operationsTableTh}>
-                            Сума(грн)
-                        </th>
-                        <th scope="col" className={styles.operationsTableTh}>
-                            Дата винесення
-                        </th>
-                        <th scope="col" className={styles.usersTableTh}>
-                            Керування
-                        </th>
+                        {tableItems.map((item) => (
+                            <th
+                                scope="col"
+                                className={styles.operationsTableTh}
+                                key={item.name}
+                                onClick={() => onSortingHandler(item.name)}
+                            >
+                                <div
+                                    className={
+                                        styles.operationsTableThHeadContainer
+                                    }
+                                >
+                                    {item.title}
+                                    {sortingKey === item.name ? (
+                                        <HiArrowsUpDown
+                                            size={14}
+                                            className={styles.sortingIcon}
+                                        />
+                                    ) : (
+                                        <></>
+                                    )}
+                                </div>
+                            </th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody className={styles.operationsTableBody}>
-                    {operations.map((operation) => (
+                    {filteredOperations?.map((operation) => (
                         <tr key={operation.id}>
                             <th
                                 scope="row"
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,18 @@
 export type User = {
-	id?: string
-	name?: string
-	username?: string
-	email?: string
-	is_admin?: boolean
-	pharmacy_number?: number
+    id?: string
+    name?: string
+    username?: string
+    email?: string
+    is_admin?: boolean
+    pharmacy_number?: number
 }
 
 export type Operation = {
-	id?: string
-	number?: string
-	user_id?: string
-	total_sum?: number
-	created_at: string
-	user?: User
+    id?: string
+    number?: string
+    pharmacy_number?: number
+    user_id?: string
+    total_sum?: number
+    created_at: string
+    user?: User
 }
